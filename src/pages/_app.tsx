e9ea13595ff106c9cd714b0a53b5d0bdf8a429fb
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-05-10 23:58:32 -0400

    style: move folders to src directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 
+import '../styles/global.css';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+
+export default function Home(): JSX.Element {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
