5ebb78571c434aca6fbd35a7a89a6ea6abc0316a
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-05-12 00:21:56 -0400

    refactor: create a CSS module to component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+export function Profile(): JSX.Element {
+  return (
+    <div>
+      <img src="github.com/fariasmateuss.png" alt="Mateus's Profile" />
+
+      <div>
+        <strong>Mateus V. Farias</strong>
+        <p>Level 4</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { ExperienceBar } from '../components/ExperienceBar';
 
+import styles from '../styles/Pages/Home.module.css';
+
 export default function Home(): JSX.Element {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
     </div>
   );
