a70729a3fafb76bfe8e799c4467cef70becbf3f4
Mateus V. Farias <me@mateusfarias.com.br>
2023-04-28 02:10:09 -0400

    feat: procedure helpers

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,54 @@
+import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
+import { type Session } from 'next-auth';
+
+import { initTRPC, TRPCError } from '@trpc/server';
+import superjson from 'superjson';
+import { getServerAuthSession } from '../auth';
+import { prisma } from '../db';
+
+type CreateContextOptions = {
+  session: Session | null;
+};
+
+const createInnerTRPCContext = (opts: CreateContextOptions) => {
+  return {
+    session: opts.session,
+    prisma,
+  };
+};
+
+export const createTRPCContext = async (opts: CreateNextContextOptions) => {
+  const { req, res } = opts;
+
+  const session = await getServerAuthSession({ req, res });
+
+  return createInnerTRPCContext({
+    session,
+  });
+};
+
+const t = initTRPC
+  .context<Awaited<ReturnType<typeof createTRPCContext>>>()
+  .create({
+    transformer: superjson,
+    errorFormatter({ shape }) {
+      return shape;
+    },
+  });
+
+export const createTRPCRouter = t.router;
+
+export const publicProcedure = t.procedure;
+
+const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+  return next({
+    ctx: {
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
